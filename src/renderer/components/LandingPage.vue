417b3a1549c7306e85ccf9972d49a12e15a92ea3
Austin Barrett <virus.cmd@gmail.com>
2019-04-19 15:18:37 -0400

    wip

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="wrapper">
+        <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
+        <main>
+        <span class="title">
+          Welcome to your new project!
+        </span>
+        </main>
+    </div>
+</template>
